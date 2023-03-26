1b0f835bee7a4bf7d80c3259532e79c3dd08f052
marty_d <martydellorusso@gmail.com>
2023-03-26 09:22:06 -0400

    Added function to display song name and index

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 const epicMix = [
-  "Time to Pretend",
+  "Midnight",
   "O-o-oh Child",
   "Wish You Were Here",
   "Heroes",
   "I Put a Spell on You",
-  "Call Me",
+  "Call Me, Maybe",
   "Paper Planes",
   "Jolene",
-  "You Don't Own Me",
-  "Fast Car",
-  "Run the World (Girls)",
-  "Superstition"
+  "Escape",
+  "Dig",
+  "Pardon Me",
+  "Superstition",
 ];
 
 const mixList = document.querySelector(".mix");
@@ -18,6 +18,18 @@ const button = document.querySelector(".show-list");
 const total = document.querySelector(".total");
 
 button.addEventListener("click", function () {
+  mixInfo(epicMix);
   mixList.classList.remove("hide");
   button.classList.add("hide");
 });
+total.innerText = `${epicMix.length} great songs!!`;
+
+const mixInfo = function (epicMix) {
+  epicMix.forEach(function (song, index) {
+    let li = document.createElement("li");
+    li.classList.add("song");
+    li.innerHTML = `<span class="song-number">${index + 1}</span>. ${song}`;
+    mixList.append(li);
+    mixList.classList.remove("hide");
+  });
+};
